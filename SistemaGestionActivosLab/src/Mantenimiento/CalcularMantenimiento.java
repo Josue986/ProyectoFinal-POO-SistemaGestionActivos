c79efb0d08d854918f181337aaec3a63c791a78f
francisco torres <ft11056575@gmail.com>
2026-07-20 16:46:14 -0500

    Agregar métodos a CalcularMantenimiento

--- SistemaGestionActivosLab/src/Mantenimiento/CalcularMantenimiento.java
+++ SistemaGestionActivosLab/src/Mantenimiento/CalcularMantenimiento.java
@@ -1,17 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Mantenimiento;
-
 /**
  *
  * @author jotue
  */
 public interface CalcularMantenimiento {
     /**
-     * Calcula el costo total o procesado de los mantenimientos.
-     * @return double mapeado desde el tipo decimal del UML.
+     * Calcula el costo total de mantenimiento.
+     * @return costo de mantenimiento
      */
     double calcularCostoMantenimiento();
+    /**
+     * Calcula el mantenimiento total.
+     * @return costo total
+     */
+    double calcularMantenimientoTotal();
+    
 }
